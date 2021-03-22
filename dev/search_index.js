78ea391fa7480aab3bbaaff8405e68bee666bfed
Documenter.jl <documenter@juliadocs.github.io>
2021-03-22 08:43:38 +0000

    build based on 9a2fcbf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BrainSim","category":"page"},{"location":"#BrainSim","page":"Home","title":"BrainSim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BrainSim]","category":"page"}]
+}
